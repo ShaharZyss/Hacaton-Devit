c81c54ec2e03b1ec4f3a3a53106ab0f77e54baec
Shahar <shahar.zyss@gmail.com>
2020-06-09 14:48:21 +0300

    second commit, try commit

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <main>
-	<h1 class="green-text" >Hello {name}!</h1>
+	<h1 class="blue-text" >Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
