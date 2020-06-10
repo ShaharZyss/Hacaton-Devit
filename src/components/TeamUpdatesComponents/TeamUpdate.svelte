c8e404255d6f64a36e4091999d8861aca13ddea4
Shahar <shahar.zyss@gmail.com>
2020-06-10 15:21:19 +0300

    Basic Team Updates

---
+++ src/components/TeamUpdatesComponents/TeamUpdate.svelte
@@ -0,0 +1,59 @@
+<script>
+  import Update from "./Update.svelte";
+
+  let accessLevel = "manager";
+  
+  let Updates = [
+    { updateTitle: "update1", updateMsg: "bli blu bla" },
+    { updateTitle: "update2", updateMsg: "bli blu bla" },
+    { updateTitle: "update3", updateMsg: "bli blu bla" }
+  ];
+
+  let newUpdateTitle = "";
+  let newUpdateMsg = "";
+
+  const handleAddition = () => {
+    Updates = [
+      ...Updates,
+      { updateTitle: newUpdateTitle, updateMsg: newUpdateMsg }
+    ];
+  };
+</script>
+
+<div class="row">
+  <div class="col s12 m4">
+    <div class="card">
+      <span class="card-title">Team Updates</span>
+      <div class="card-content">
+        <ul class="collapsible">
+          {#each Updates as update, i (i)}
+            <Update {update} />
+          {/each}
+        </ul>
+      </div>
+      {#if accessLevel == 'manager'}
+        <div class="card-action">
+          <div class="input-field inline s12 m5">
+            <input
+              id="add-update-Title"
+              type="text"
+              class="validate"
+              bind:value={newUpdateTitle} />
+            <label for="add-update-Title">Update Title</label>
+          </div>
+          <div class="input-field inline s12 m5">
+            <input
+              id="add-update-Msg"
+              type="text"
+              class="validate"
+              bind:value={newUpdateMsg} />
+            <label for="add-update-Msg">Update Message</label>
+          </div>
+          <a href="#!" on:click={handleAddition}>
+            <i class="material-icons green-text">add</i>
+          </a>
+        </div>
+      {/if}
+    </div>
+  </div>
+</div>
